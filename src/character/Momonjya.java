81bed9203c18d793aed2d1e61c11700c843d3428
hinako <hinako@ndg-no-mbp>
2020-10-22 17:38:54 +0900

    クラス名変更

--- src/character/Momonjya.java
+++ src/character/Momonjya.java
@@ -2,16 +2,16 @@ package character;
 
 import java.util.Random;
 
-public class momonjya extends Monster {
+public class Momonjya extends Monster {
 	Random rand = new Random();
 
-	public momonjya() {
+	public Momonjya() {
 		this.name = "謎のももんじゃ";
 		this.hp = 774;
 		this.individual = 'X';
 	}
 
-	public momonjya(char individual) {
+	public Momonjya(char individual) {
 		this.name = "ももんじゃ";
 		this.hp = 138;
 		this.individual = individual;
