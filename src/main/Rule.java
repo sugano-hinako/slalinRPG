0daaa27463fca1ce57cd5200f9ffa479f6edc13c
hinako <hinako@ndg-no-mbp>
2020-10-23 17:17:10 +0900

    インスタンス生成　playerActionメソッド作成（途中）

--- src/main/Rule.java
+++ src/main/Rule.java
@@ -1,6 +1,36 @@
 package main;
 
+import character.Momonjya;
+import character.Otaota;
+import character.Player;
+
 public class Rule {
+	//今回登場のプレイヤーとモンスター's
+	Player pl = new Player();
+	Momonjya mo = new Momonjya('A');
+	Otaota ota = new Otaota('∞');
+
 	//なんのモンスターが出てきたか表示する
+	public void showMonster() {
+		System.out.println(mo.name + "　と　" + ota.name + "がとびだしてきた！！");
+	}
+
+	//Player
+	public void playerAction() {
+		//何をするかの指定
+		pl.setAction();
+		pl.actionName();
+		pl.action();
+		if(pl.action == 1) {
+			pl.choiceMonster();
+			pl.chMonster();
+			if(pl.chMonster == 1) {
+				pl.setChoice();
+				pl.choiceName();
+				pl.choice();
+			}
+		}
+	}
+
 
 }
