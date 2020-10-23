b1b25d093cc2881d7b373e3e9cb2dcc9305817b7
hinako <hinako@ndg-no-mbp>
2020-10-23 17:21:49 +0900

    インスタンス生成　startメソッド作成

--- src/main/GameControll.java
+++ src/main/GameControll.java
@@ -1,7 +1,26 @@
 package main;
 
-public class GameControll {
+import character.Momonjya;
+import character.Otaota;
+import character.Player;
 
+public class GameControll {
+	private Player plSlalin;
+	private Momonjya moMomonjya;
+	private Otaota moOtaota;
+
+	public GameControll() {
+		this.plSlalin = new Player();
+		this.moMomonjya = new Momonjya();
+		this.moOtaota = new Otaota();
+	}
+
+	//ゲームスタート
+	public void start() {
+		System.out.println(this.plSlalin.name +"の冒険(簡易版)スタート！");
+		System.out.println("なにかがくさかげからとび出してきた！");
+		System.out.println("");
+	}
 
 
 }
