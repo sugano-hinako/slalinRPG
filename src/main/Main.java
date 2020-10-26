482df0b5f213bed37ddab275a444c6f6ce51c793
hinako <hinako@ndg-no-mbp>
2020-10-26 14:22:47 +0900

    テスト文

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,13 @@
 package main;
 
-import character.Momonjya;
-
 public class Main {
 
 	public static void main(String[] args) {
 		Rule r = new Rule();
-		Momonjya m = new Momonjya('B');
 
 		r.playerAction();
+		r.monsterAction();
+
 
 	}
 
