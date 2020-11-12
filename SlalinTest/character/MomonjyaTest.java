30e0f7ea40c062bc982acf3b4e7838553fe46d23
hinako <hinako@ndg-no-mbp>
2020-11-12 14:07:27 +0900

    assertEqualsの期待値の箇所がエラーになる

--- SlalinTest/character/MomonjyaTest.java
+++ SlalinTest/character/MomonjyaTest.java
@@ -6,10 +6,17 @@ import org.junit.Test;
 
 public class MomonjyaTest {
 	@Test
-	public void testMomonjya(){
+	public void testMomonjyaRun(){
 		Momonjya mo = new Momonjya('X');
 		String runmsg = mo.run();
 		assertEquals(mo.name + "は逃げ出した！",runmsg);
+	}
+	@Test
+	public void testMomonjyaAttack() {
+		Momonjya mo = new Momonjya('X');
+		Player p = new Player();
+		int atdmg = mo.attack(p);
+		assertEquals(da,atdmg);
 	}
 
 }
